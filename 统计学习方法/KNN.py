ba01f6f57622951f8de634b35bbb3b663b2bd5a8
cccccent <cls300016@gmail.com>
2020-08-15 14:13:01 +0800

    Add files via upload
    add some annotation

--- 统计学习方法/KNN.py
+++ 统计学习方法/KNN.py
@@ -13,17 +13,24 @@ from numpy import *
 import operator
 
 def createDataSet():
+    # 创建数据集及标签，实际应用可将此部分替换为对目标数据的处理
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group,labels
 
 def classify(inX,dataSet,labels,k):
+    # 分类器
+    # dataSetSize 为数据集大小
     dataSetSize = dataSet.shape[0]
+    # diffMat 为目标数据和数据集中点的坐标差
     diffMat = tile(inX,(dataSetSize,1))-dataSet
+    # 计算欧式距离
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis = 1)
     distances = sqDistances**0.5
+    # 将距离排序
     sortedDistIndicies = distances.argsort()
+    # 遍历取出前k个点，利用其类别投票
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
@@ -32,8 +39,11 @@ def classify(inX,dataSet,labels,k):
     return sortedClassCount[0][0]
 
 group,labels = createDataSet()
+print("DataSet:\n-------\nGroup:")
 print(group)
+print("------\nLabels:")
 print(labels)
 inA = [0.7,0.7]
+print("------\nResult:")
 labelOfA = classify(inA,group,labels,1)
 print(labelOfA)
